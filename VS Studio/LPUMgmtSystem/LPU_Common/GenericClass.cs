46bca2af17b82c7e94c18242490c1c92c669cc10
Aryan <aryaneqqbed@gmail.com>
2026-01-06 11:47:27 +0530

    New Update

---
+++ VS Studio/LPUMgmtSystem/LPU_Common/GenericClass.cs
@@ -0,0 +1,7 @@
+﻿namespace LPU_Common
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ VS Studio/LPUMgmtSystem/LPU_Exceptions/LPUException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LPU_Exceptions
+{
+    /// <summary>
+    /// Custom Exception Class for LPU Management System
+    /// By Aryan on 29/12/2025 at 11:34 AM
+    /// </summary>
+    public class LPUException : Exception
+    {
+        public LPUException() : base()
+        {
+            
+        }
+
+        public LPUException(string message) : base(message)
+        {
+            
+        }
+
+        public LPUException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
+
+
+    }
+}
