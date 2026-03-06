ac45d425228680120150272f08bf1fc0940ae4f0
Aryan <aryaneqqbed@gmail.com>
2026-03-06 10:10:27 +0530

    Update

--- VS Studio/Gold_Price_Pridiction/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gold_Price_Pridiction.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
